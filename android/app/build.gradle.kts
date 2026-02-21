d9a4de813033f22db3e090c67bc1935ee4d333fe
Albert Theodore Lutterodt <theodore@Alberts-MacBook-Pro.local>
2026-02-21 13:36:58 +0000

    refactor: rename app from FakeStore to MiniStore
    - Update Android package namespace to com.ministore.mini_store_app
    - Update iOS bundle identifier to com.ministore.miniStoreApp
    - Rename Android package directory structure
    - Update all app identifiers consistently across platforms

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.fakestore.fake_store_app"
+    namespace = "com.ministore.mini_store_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.fakestore.fake_store_app"
+        applicationId = "com.ministore.mini_store_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
